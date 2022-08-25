d449467ed632056000d39e2efaeaf46bf393c43c
Keivan <keyvan0082@gmail.com>
2022-08-25 21:21:58 +0430

    Visualize graph

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,24 @@
-console.log(Trie);
+const viz = new Viz();
+const body = document.querySelector("body");
+const mainDiv = document.createElement("div");
+mainDiv.id = "mainDiv";
+body.appendChild(mainDiv);
 const graph = Trie.generateTrieGraph("tap", "taps", "tup", "tups");
-console.log(graph);
-console.log(Viz);
+const graphInViz = graph.convertToVizGraph();
+(async () => {
+    const svg = await viz.renderSVGElement(graphInViz);
+    styleGraph(svg);
+    console.log(svg);
+})();
+
+const styleGraph = (svg) => {
+    svg.querySelectorAll("polygon + text").forEach((e) => {
+        if (e.innerHTML == "EOW") return e.setAttribute("font-size", "10pt");
+        e.setAttribute("font-size", "18pt");
+        const x = +e.getAttribute("x");
+        e.setAttribute("x", x + 1);
+    });
+    svg.querySelectorAll("ellipse + text").forEach((n) => (n.innerHTML = " "));
+    mainDiv.style.width = svg.getAttribute("width");
+    mainDiv.appendChild(svg);
+};
