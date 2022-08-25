4be16e0aa76e3bbad7bf31484aa568321b79736a
Keivan <keyvan0082@gmail.com>
2022-08-25 21:07:26 +0430

    Build dist folder via `tsc`/`webpack`

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
 import trie from "./lib";
-
-const t = trie.generateTrieGraph("tops", "top", "tap", "taps");
-const m2 = trie.generateDawg("tops", "top", "tap", "taps");
-console.dir(
-    t.getShortRootSummary(),
-    {
-        depth: 25,
-    }
-);
-
-console.log(t.convertToVizGraph())
+//@ts-ignore
+window.Trie = trie;
