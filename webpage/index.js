680a7aa65450d8c08a6b3d406ac7344afaaf0b98
Keivan <keyvan0082@gmail.com>
2022-08-26 19:52:44 +0430

    Convert build div to `button`

--- webpage/index.js
+++ webpage/index.js
@@ -8,7 +8,7 @@ const dawgTitle = document.querySelector(".dawg-graph .title");
  * @type {HTMLInputElement}
  */
 const userInput = document.querySelector(".inputContainer input[type=text]");
-const buildBtn = document.querySelector(".build-btn>div");
+const buildBtn = document.querySelector(".build-btn>button");
 (async () => {
     buildGraphs(["cat", "cats", "car", "cars"], {
         trieEL,
